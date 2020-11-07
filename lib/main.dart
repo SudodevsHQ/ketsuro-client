90a02e2252fb06ba85a33ef12bdf3f73173acc8c
Anurag Patil <me@metaboy.info>
2020-11-07 13:44:40 +0530

    feat: splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ketsuro/common/colors.dart';
+import 'package:relative_scale/relative_scale.dart';
 import 'package:youtube_api/youtube_api.dart';
 
 import 'config/config.dart';
+import 'screens/splash.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,86 +13,101 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DemoApp(),
-    );
-  }
-}
-
-class DemoApp extends StatefulWidget {
-  @override
-  _DemoAppState createState() => _DemoAppState();
-}
-
-class _DemoAppState extends State<DemoApp> {
-  static String key = youtubeApiKey;
-
-  YoutubeAPI ytApi = YoutubeAPI(key);
-  List<YT_API> ytResult = [];
-
-  callAPI() async {
-    String query = "Java";
-    ytResult = await ytApi.search(query);
-    ytResult = await ytApi.nextPage();
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    callAPI();
-    print('hello');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Youtube API'),
-      ),
-      body: Container(
-        child: ListView.builder(
-          itemCount: ytResult.length,
-          itemBuilder: (_, int index) => listItem(index),
-        ),
-      ),
-    );
-  }
-
-  Widget listItem(index) {
-    return Card(
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 7.0),
-        padding: EdgeInsets.all(12.0),
-        child: Row(
-          children: <Widget>[
-            Image.network(
-              ytResult[index].thumbnail['default']['url'],
-            ),
-            Padding(padding: EdgeInsets.only(right: 20.0)),
-            Expanded(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                  Text(
-                    ytResult[index].title,
-                    softWrap: true,
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                  Padding(padding: EdgeInsets.only(bottom: 1.5)),
-                  Text(
-                    ytResult[index].channelTitle,
-                    softWrap: true,
-                  ),
-                  Padding(padding: EdgeInsets.only(bottom: 3.0)),
-                  Text(
-                    ytResult[index].url,
-                    softWrap: true,
-                  ),
-                ]))
-          ],
+      home: Ketsuro(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ).copyWith(
+          body1: TextStyle(
+            color: ketsuroBlack,
+          ),
+          body2: TextStyle(
+            color: ketsuroBlack,
+          ),
         ),
       ),
     );
   }
 }
+
+
+
+// class DemoApp extends StatefulWidget {
+//   @override
+//   _DemoAppState createState() => _DemoAppState();
+// }
+
+// class _DemoAppState extends State<DemoApp> {
+//   static String key = youtubeApiKey;
+
+//   YoutubeAPI ytApi = YoutubeAPI(key);
+//   List<YT_API> ytResult = [];
+
+//   callAPI() async {
+//     String query = "Java";
+//     ytResult = await ytApi.search(query);
+//     ytResult = await ytApi.nextPage();
+//     setState(() {});
+//   }
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     callAPI();
+//     print('hello');
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Youtube API'),
+//       ),
+//       body: Container(
+//         child: ListView.builder(
+//           itemCount: ytResult.length,
+//           itemBuilder: (_, int index) => listItem(index),
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget listItem(index) {
+//     return Card(
+//       child: Container(
+//         margin: EdgeInsets.symmetric(vertical: 7.0),
+//         padding: EdgeInsets.all(12.0),
+//         child: Row(
+//           children: <Widget>[
+//             Image.network(
+//               ytResult[index].thumbnail['default']['url'],
+//             ),
+//             Padding(padding: EdgeInsets.only(right: 20.0)),
+//             Expanded(
+//                 child: Column(
+//                     mainAxisAlignment: MainAxisAlignment.start,
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: <Widget>[
+//                   Text(
+//                     ytResult[index].title,
+//                     softWrap: true,
+//                     style: TextStyle(fontSize: 18.0),
+//                   ),
+//                   Padding(padding: EdgeInsets.only(bottom: 1.5)),
+//                   Text(
+//                     ytResult[index].channelTitle,
+//                     softWrap: true,
+//                   ),
+//                   Padding(padding: EdgeInsets.only(bottom: 3.0)),
+//                   Text(
+//                     ytResult[index].url,
+//                     softWrap: true,
+//                   ),
+//                 ]))
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
